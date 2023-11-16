57425e4ad616603ba246b41dce4b8bc27507e386
Chris Knowles <chris_knowles@waters.com>
2023-11-16 21:18:43 +0000

    Added attack sequence decorator and attack selection

--- main.py
+++ main.py
@@ -4,10 +4,34 @@ import itertools
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 import time
 
+def attack_selector(password):
+    print('Welcome to the dictionary / brute force attack simulator.  Select one of the following options:\n')
+    selection = input('1 - Run a dictionary attack using the 14 million most common passwords from the 2009 RockYou.txt leak \n'
+                      '2 - Run a brute force attack using only symbols and numbers \n enter number:')
+    if selection == '1':
+        dictionary_attack(password)
+        return
+    if selection == '2':
+        brute_force(password)
+        return
+    else:
+        print('not a valid entry, please select one of the following options')
+        attack_selector(password)
 
 
+def attack_sequencer(func):
+    def attack(password):
+        print(f'Running {func.__name__}...\n')
+        start_time = time.time()
+        func(password)
+        end_time = time.time()
+        execution_time = end_time - start_time
+        print(f'{func.__name__} complete in in {round(execution_time, 4)} seconds \n')
+
+    return attack
+
+@attack_sequencer
 def dictionary_attack(password):
-    start_time = time.time()
 
     with open('rockyou.txt',encoding='latin1') as file:
         lines = [line.strip('\n') for line in file]
@@ -16,19 +40,13 @@ def dictionary_attack(password):
     for index, line in enumerate(lines):
         if line == password:
             print(f'password found at line {index + 1}!')
-            end_time = time.time()
-            execution_time = end_time - start_time
-            print(f'Password found in {round(execution_time, 4)} seconds')
-
             return
 
     else:
-        print(f'Password was not found in any of the {len(lines)} entries!  Try again?')
-        password = input('Enter password Password:> ')
-        dictionary_attack(password)
-
+        print(f'Password was not found in any of the {len(lines)} entries')
+        attack_selector(password)
+@attack_sequencer
 def brute_force(password):
-    start_time = time.time()
     lowercase_chars = [chr(i) for i in range (ord('a'), ord('z')+1)]
     uppercase_chars = [chr(i) for i in range(ord('A'), ord('Z') + 1)]
     number_chars = [chr(i) for i in range(ord('0'), ord('9') + 1)]
@@ -39,10 +57,7 @@ def brute_force(password):
             guess = ''.join(guess)
             print(f'checking password {index + 1}, current guess = \'{guess}\'')
             if guess == password:
-                end_time = time.time()
-                execution_time = end_time - start_time
-                print(f'password \'{guess}\' brute forced in {round(execution_time, 4)} seconds at iteration {index + 1}!')
-
+                print(f'password \'{guess}\' brute forced in {index + 1} attempts!')
                 return
 
 # TODO
@@ -53,12 +68,13 @@ def brute_force(password):
 #Expand on your program to either do a dictionary/brute force attack. Hint: you can use args to do this ;)
 #how to point at doms server?
 def main():
-    print('Can you guess the most common password from the 2009 \'RockYou\' data leak?')
     password = input('Enter password Password:> ')
 
 
-    brute_force(password)
-    dictionary_attack(password)
+    attack_selector(password)
+    # dictionary_attack(password)
+    # brute_force(password)
+
 
 
 if __name__ == '__main__':
